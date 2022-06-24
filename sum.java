a66e69e56585a24daab235193f55d856036c8ca2
chetan061292 <chetan@tecorelabs.com>
2022-06-24 14:35:16 +0530

    added massage

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(string[] args) {
         int a = 5;
         int b = 6;
-        system.out.println(a+b);
+        system.out.println("this is sum" a+b);
     }
 }
